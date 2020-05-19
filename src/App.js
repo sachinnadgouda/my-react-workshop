6ab571ce21ee1d99108db4458c6d104653fdb55a
Sachin Nadgouda <sachinnadgouda@gmail.com>
2020-05-19 16:44:56 +0200

    f1: added show hide for persons, added input for tow way binding

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
@@ -10,55 +9,70 @@ class App extends Component {
             {name: 'sachin', age: 28},
             {name: 'Chaitra', age: 29},
             {name: 'Test', age: 30}
-        ]
+        ],
+        showPersons: false,
     };
 
-    changeStateHandler = () => {
+    changeStateHandler = (newName) => {
         const state = {
             persons: [
-                {name: 'sachin', age: Math.floor( Math.random() * 30 )},
-                {name: 'Chaitra', age: Math.floor( Math.random() * 30 )},
-                {name: 'Test', age: Math.floor( Math.random() * 30 )}
+                {name: newName, age: Math.floor(Math.random() * 30)},
+                {name: 'Chaitra', age: Math.floor(Math.random() * 30)},
+                {name: 'Test', age: Math.floor(Math.random() * 30)}
             ]
         };
         this.setState({...state});
         console.log("was clicked");
     };
 
+    changeNameHandler = (event) => {
+        const state = {
+            persons: [
+                {name: event.target.value, age: Math.floor(Math.random() * 30)},
+                {name: 'Chaitra', age: Math.floor(Math.random() * 30)},
+                {name: 'Test', age: Math.floor(Math.random() * 30)}
+            ]
+        };
+        this.setState({...state});
+        console.log("was clicked");
+    };
+
+    filterResultsHandler = () => {
+        this.setState({showPersons: !this.state.showPersons})
+    };
+
     render() {
+        const style = {
+            color: 'blue',
+        };
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                    <Person
-                        name={this.state.persons[0].name}
-                        age={this.state.persons[0].age}
-                        changeStateHandler={this.changeStateHandler}
-                    >
-                        My hobbies: Cricket and Football
-                    </Person>
-                    <Person
-                        name={this.state.persons[1].name}
-                        age={this.state.persons[1].age}
-                        changeStateHandler={this.changeStateHandler}
-                    />
-                    <Person
-                        name={this.state.persons[2].name}
-                        age={this.state.persons[2].age}
-                        changeStateHandler={this.changeStateHandler}
-                    />
-                </header>
+                <h1 style={style}>This is a react app</h1>
+                <button onClick={this.filterResultsHandler}>Filter</button>
+                { this.state.showPersons ?
+                    <div >
+                        <Person
+                            name={this.state.persons[0].name}
+                            age={this.state.persons[0].age}
+                            changeStateHandler={() => this.changeStateHandler("Test 1")}
+                            changeNameHandler={this.changeNameHandler}
+                        >
+                            My hobbies: Cricket and Football
+                        </Person>
+                        <Person
+                            name={this.state.persons[1].name}
+                            age={this.state.persons[1].age}
+                            changeStateHandler={this.changeStateHandler.bind(this, "Test 2")}
+                            changeNameHandler={this.changeNameHandler}
+                        />
+                        <Person
+                            name={this.state.persons[2].name}
+                            age={this.state.persons[2].age}
+                            changeStateHandler={this.changeStateHandler.bind(this, "Test 3")}
+                            changeNameHandler={this.changeNameHandler}
+                        />
+                    </div> : null
+                }
             </div>
         );
     }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './Person.css';
 
 // stateless
 const person = ( props ) => {
     return (
-        <div>
+        <div className="person">
             <p onClick={props.changeStateHandler}> I am {props.name} and {props.age} years old.</p>
             <p> {props.children} </p>
+            <input type="text" value={props.name} onChange={props.changeNameHandler}/>
         </div>
     )
 };
